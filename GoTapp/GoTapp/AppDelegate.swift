a93df1b9ac3ffeaf99b98d8a67357823bb72f153
Ricardo González Pacheco <rigonpa@gmail.com>
2020-02-16 11:25:15 +0100

    Eliminación storyboard + creo ventana/escena

--- GoTapp/GoTapp/AppDelegate.swift
+++ GoTapp/GoTapp/AppDelegate.swift
@@ -12,10 +12,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    // MARK: Mis estados
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch. Que esto sea lo más pequeño posible para que la aplicación arranque lo antes posible
+        
+        print("Estoy apunto de ser ejecutado")
+        
+        //let strin = "Nombre"
+        //let mayus = strin.uppercased()
+        
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("Me acaban de mandar al fondo del móvil. Vaya tela")
     }
 
     // MARK: UISceneSession Lifecycle
