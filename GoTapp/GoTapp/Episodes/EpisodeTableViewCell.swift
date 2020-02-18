a97520a7c19f00a95a88a58b71d8afddeaf36ddc
Ricardo González Pacheco <rigonpa@gmail.com>
2020-02-18 13:59:47 +0100

    Creación tabla y sus celdas para pantalla episodios

---
+++ GoTapp/GoTapp/Episodes/EpisodeTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  EpisodeTableViewCell.swift
+//  GoTapp
+//
+//  Created by Ricardo González Pacheco on 18/02/2020.
+//  Copyright © 2020 Ricardo González Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTableViewCell: UITableViewCell {
+   
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var rateButton: UIButton!
+    
+    override func awakeFromNib() {
+        thumb.layer.cornerRadius = 2.0
+        thumb.layer.borderColor = UIColor.white.withAlphaComponent(0.2).cgColor
+        thumb.layer.borderWidth = 1.0
+        rateButton.layer.cornerRadius = 15
+    }
+    
+}
--- GoTapp/GoTapp/Episodes/EpisodeViewController.swift
+++ GoTapp/GoTapp/Episodes/EpisodeViewController.swift
@@ -9,28 +9,50 @@
 import UIKit
 
 class EpisodeViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setup()
+    }
+    
+    func setup() {
+        
         self.title = "Seasons"
-
-        // Do any additional setup after loading the view.
+        
+        let nib = UINib.init(nibName: "EpisodeTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "EpisodeTableViewCell")
     }
     
     var episodes: [Episode] = [Episode.init(id: 1, name: "Winter is Coming", date: "April 17, 2011", image: "episodeTest", episode: 1, season: 1, overview: "Jon Arryn, the Hand of the King, is dead. King Robert…")]
     
-    @IBAction func openRate(_ sender: Any) {
-        //Código para abrir pantalla Rate
-        let rateViewController = RateViewController()
-        
-        //Para cambiar de transición en el present:
-        rateViewController.modalTransitionStyle = .crossDissolve
-        rateViewController.modalPresentationStyle = .fullScreen
-        
-        self.present(rateViewController, animated: true, completion: nil)
-        //self.navigationController?.pushViewController(rateViewController, animated: true)
-        
-        
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    @IBAction func openRate(_ sender: Any) {
+//        //Código para abrir pantalla Rate
+//        let rateViewController = RateViewController()
+//
+//        //Para cambiar de transición en el present:
+//        rateViewController.modalTransitionStyle = .crossDissolve
+//        rateViewController.modalPresentationStyle = .fullScreen
+//
+//        self.present(rateViewController, animated: true, completion: nil)
+//        //self.navigationController?.pushViewController(rateViewController, animated: true)
+//
+//
+//    }
     
 }
